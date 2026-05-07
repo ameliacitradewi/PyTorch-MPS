928542355d54ff8c999829677572b2bb58bb6ab1
Amelia Citra <ameliacitradewi@gmail.com>
2026-05-07 16:32:53 +0700

    Initial Commit

---
+++ WebCam/WebCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebCamApp.swift
+//  WebCam
+//
+//  Created by Amelia Citra on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WebCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
